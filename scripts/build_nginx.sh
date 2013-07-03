12556fd72d12ae80e9feac392bfc339286b3400f
Ori Peleg <nospam@orip.org>
2013-07-04 02:55:17 +0300

    cosmetics

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -5,14 +5,11 @@
 
 NGINX_VERSION=1.4.1
 PCRE_VERSION=8.21
-NGINX_TARBALL_URL=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-PCRE_TARBALL_URL=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.bz2
 
-script_dir=$(cd $(dirname $0); pwd)
+nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.bz2
+
 temp_dir=$(mktemp -d /tmp/vulcan_nginx.XXXXXXXXXX)
-
-VULCAN_ARCHIVE_RESULT=$temp_dir/nginx-${NGINX_VERSION}-built-with-vulcan.tar.gz
-
 
 cleanup() {
   echo "Cleaning up $temp_dir"
@@ -21,18 +18,27 @@ cleanup() {
 }
 trap cleanup EXIT
 
+script_dir=$(cd $(dirname $0); pwd)
+vulcan_archive_result=$temp_dir/nginx-${NGINX_VERSION}-built-with-vulcan.tar.gz
+untarring_dir=$temp_dir/untarring
+nginx_binary_drop_dir=$script_dir/../bin
+
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
-echo "Downloading $NGINX_TARBALL_URL"
-curl $NGINX_TARBALL_URL | tar xf -
+echo "Downloading $nginx_tarball_url"
+curl $nginx_tarball_url | tar xf -
 
-echo "Downloading $PCRE_TARBALL_URL"
-(cd nginx-${NGINX_VERSION} && curl $PCRE_TARBALL_URL | tar xf -)
+echo "Downloading $pcre_tarball_url"
+(cd nginx-${NGINX_VERSION} && curl $pcre_tarball_url | tar xf -)
 
-vulcan build -o ${VULCAN_ARCHIVE_RESULT} -s nginx-${NGINX_VERSION} -v -p /tmp/nginx -c "./configure --with-pcre=pcre-${PCRE_VERSION} --prefix=/tmp/nginx && make install"
+vulcan build -o ${vulcan_archive_result} -s nginx-${NGINX_VERSION} -v -p /tmp/nginx -c "./configure --with-pcre=pcre-${PCRE_VERSION} --prefix=/tmp/nginx && make install"
 
-mkdir -p $temp_dir/untarring
-cd $temp_dir/untarring
-tar -xf $VULCAN_ARCHIVE_RESULT
-cp sbin/nginx $script_dir/../bin
+echo "Extracting the nginx binary into the buildback"
+mkdir -p $untarring_dir
+cd $untarring_dir
+tar -xf $vulcan_archive_result
+mkdir -p $nginx_binary_drop_dir
+cp sbin/nginx $nginx_binary_drop_dir
+
+echo "Copied the nginx binary into $(cd $nginx_binary_drop_dir; pwd)"
