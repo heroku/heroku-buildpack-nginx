005ca0374e3cf61a29fb0f9041a7315677af1972
Juan C. Müller <jcmuller@gmail.com>
2014-08-27 11:28:56 -0400

    Allow version of utilities to be set on environment

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,10 +9,9 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=1.5.7
-PCRE_VERSION=8.21
-HEADERS_MORE_VERSION=0.23
-
+NGINX_VERSION=${NGINX_VERSION-1.5.7}
+PCRE_VERSION=${PCRE_VERSION-8.21}
+HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
